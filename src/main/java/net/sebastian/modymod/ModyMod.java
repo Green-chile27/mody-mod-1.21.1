e029fc3a39fd2e31cfc26cac7057a700ebe42854
seb <armoredindustries@duck.com>
2026-02-24 09:50:28 -0700

    do it

---
+++ src/main/java/net/sebastian/modymod/ModyMod.java
@@ -0,0 +1,16 @@
+package net.sebastian.modymod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ModyMod implements ModInitializer {
+	public static final String MOD_ID = "modymod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/sebastian/modymod/ModyModClient.java
@@ -0,0 +1,10 @@
+package net.sebastian.modymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
