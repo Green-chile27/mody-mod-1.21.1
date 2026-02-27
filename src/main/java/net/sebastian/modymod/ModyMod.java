83ccbde1ef6f43643d4be07ceb609bd2fff5b242
seb <armoredindustries@duck.com>
2026-02-27 13:38:52 -0700

    Added Garnet and Raw Garnet

--- src/main/java/net/sebastian/modymod/ModyMod.java
+++ src/main/java/net/sebastian/modymod/ModyMod.java
@@ -2,6 +2,7 @@ package net.sebastian.modymod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.sebastian.modymod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class ModyMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/sebastian/modymod/item/ModItems.java
@@ -0,0 +1,28 @@
+package net.sebastian.modymod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.sebastian.modymod.ModyMod;
+
+public class ModItems {
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(ModyMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        ModyMod.LOGGER.info("Registering ModItems for" + ModyMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PINK_GARNET);
+            entries.add(RAW_PINK_GARNET);
+            //fabricItemGroupEntries.add(PINK_GARNET); //If it doesn;t work this migh be why (change it to fabricEntries)
+        });
+    }
+}
