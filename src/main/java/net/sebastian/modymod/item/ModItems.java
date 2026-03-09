d838bf63c8fc22b17264f24a6424e1e50f0a3200
seb <armoredindustries@duck.com>
2026-03-09 11:03:40 -0600

    Added pork sandwich, and replaced raw garnet w raw limestone

--- src/main/java/net/sebastian/modymod/item/ModItems.java
+++ src/main/java/net/sebastian/modymod/item/ModItems.java
@@ -13,10 +13,10 @@ public class ModItems {
 
     //These two items are just examples items from tutorial (Delete later)
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
-    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
 
     //My actual custom items:
     public static final Item PORK_SANDWICH = registerItem("pork_sandwich", new Item(new Item.Settings()));
+    public static final Item RAW_LIMESTONE = registerItem("raw_limestone", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(ModyMod.MOD_ID, name), item);
@@ -25,12 +25,12 @@ public class ModItems {
     public static void registerModItems() {
         ModyMod.LOGGER.info("Registering ModItems for" + ModyMod.MOD_ID);
 
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> { //add custom items to the Ingredients menu
             entries.add(PINK_GARNET);
-            entries.add(RAW_PINK_GARNET);
+            entries.add(RAW_LIMESTONE);
         });
 
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> { //add custom items to food menu
             entries.add(PORK_SANDWICH);
         });
     }
