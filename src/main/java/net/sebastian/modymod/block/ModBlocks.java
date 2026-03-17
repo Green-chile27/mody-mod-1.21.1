1ac53e0b2c2019e1135a281d05e166a5d4ec4a37
seb <armoredindustries@duck.com>
2026-03-17 10:54:43 -0600

    Added limestone ore and limestone deepslate ore, then also added drops and loot tables for ores and raw limestone block

--- src/main/java/net/sebastian/modymod/block/ModBlocks.java
+++ src/main/java/net/sebastian/modymod/block/ModBlocks.java
@@ -2,6 +2,7 @@ package net.sebastian.modymod.block;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -9,6 +10,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.sebastian.modymod.ModyMod;
 import net.minecraft.block.Block;
 
@@ -18,6 +20,15 @@ public class ModBlocks {
     public static final Block RAW_LIMESTONE_BLOCK = registerBlock("raw_limestone_block",
             new Block(AbstractBlock.Settings.create().strength(2f)
                     .requiresTool().sounds(BlockSoundGroup.CALCITE)));
+
+    //limestone ore block for natural generation
+    public static final Block LIMESTONE_ORE = registerBlock("limestone_ore",
+        new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                AbstractBlock.Settings.create().strength(3f).requiresTool()));
+    //deepslate variation
+    public static final Block LIMESTONE_DEEPSLATE_ORE = registerBlock("limestone_deepslate_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(3, 6),
+                    AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
 
     //Credit to Modding by Kaupenjoe for these helper methods to register the blocks
     private static Block registerBlock(String name, Block block) {
@@ -35,6 +46,8 @@ public class ModBlocks {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
             entries.add(ModBlocks.RAW_LIMESTONE_BLOCK);
+            entries.add(ModBlocks.LIMESTONE_ORE);
+            entries.add(ModBlocks.LIMESTONE_DEEPSLATE_ORE);
         });
     }
 }
